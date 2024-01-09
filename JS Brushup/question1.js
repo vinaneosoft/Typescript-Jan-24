cc7fbc36df7b5d8cde5ee8a48c020671a9a090c0
kokanevina <kokane.vina@gmail.com>
2024-01-09 16:42:39 +0530

    assignment implementation

--- JS Brushup/question1.js
+++ JS Brushup/question1.js
@@ -1,6 +1,19 @@
 
 let employees=["Amol",'aruna','Komal','RamP','Arnavi',"Rajamp", 'Anup'];
 
+
+let startsWithA=[];
+for(let employee of employees){
+    if(employee.toLowerCase().startsWith('a'))
+        startsWithA.push(employee);
+}
+console.log("Employees starting with A/a ",startsWithA);
+
+
+
+
+
+
 let endsWithP="";
 for(let employee of employees){
     if(employee.toLowerCase().endsWith('p')){
