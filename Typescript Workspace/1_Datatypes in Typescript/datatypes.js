47a950b705195fe1e66ab4b98eb81f4e11a8541d
kokanevina <kokane.vina@gmail.com>
2024-01-10 17:14:05 +0530

    data types

---
+++ Typescript Workspace/1_Datatypes in Typescript/datatypes.js
@@ -0,0 +1,5 @@
+var companyName; // strictly typed lang
+//companyName=890;  //incompatible value
+companyName = "NEOSOFT";
+//companyName=true; 
+console.log(companyName);
--- Typescript Workspace/1_Datatypes in Typescript/datatypes.ts
+++ Typescript Workspace/1_Datatypes in Typescript/datatypes.ts
@@ -1,5 +1,22 @@
 var companyName:string ;  // strictly typed lang
-
-companyName=890;  //incompatible value
+ //companyName=890;  //incompatible value
 companyName="NEOSOFT"
-companyName=true;
+//companyName=true; 
+console.log(companyName);
+
+let salary:number;
+salary=78000;
+
+let marriedStatus:boolean
+marriedStatus=true;
+
+let employeeId:any
+employeeId=890;
+employeeId="e890";
+
+let ss : string | number;
+
+let branch="Rabale";
+//branch=8999;
+
+let contactNumber;contactNumber=7878787878;
