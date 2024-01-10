cc7457713d5f2b7d6a69b8a534e3cef683c2ed16
kokanevina <kokane.vina@gmail.com>
2024-01-10 16:28:48 +0530

    datatypes

---
+++ Typescript Workspace/1_Datatypes in Typescript/test.js
@@ -0,0 +1,8 @@
+var companyName;  // dynamically types lang
+
+companyName=890;
+console.log(typeof companyName); // number
+companyName="NEOSOFT";
+console.log(typeof companyName); // string
+companyName=true;
+console.log( typeof companyName); // boolean
