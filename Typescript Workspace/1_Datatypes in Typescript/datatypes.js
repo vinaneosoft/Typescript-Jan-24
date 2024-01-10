366650d0ee8c72a39b14905598066d0ccfa6ff8c
kokanevina <kokane.vina@gmail.com>
2024-01-10 17:51:12 +0530

    datatypes

--- Typescript Workspace/1_Datatypes in Typescript/datatypes.js
+++ Typescript Workspace/1_Datatypes in Typescript/datatypes.js
@@ -3,3 +3,41 @@ var companyName; // strictly typed lang
 companyName = "NEOSOFT";
 //companyName=true; 
 console.log(companyName);
+var salary;
+salary = 78000;
+var marriedStatus;
+marriedStatus = true;
+var employeeId;
+employeeId = 890;
+employeeId = "e890";
+var ss;
+var branch = "Rabale";
+//branch=8999;
+var contactNumber;
+contactNumber = 7878787878;
+var headoffice;
+headoffice = "Dadar";
+console.log(headoffice.length);
+console.log(headoffice.charAt(0));
+var sss;
+sss = "hello";
+var employee = {
+    id: 567,
+    name: "hari",
+    salary: 78000
+};
+console.log(employee.id);
+console.log(employee.name);
+console.log(employee.salary);
+for (var key in employee) {
+    console.log(key);
+    console.log(employee[key]);
+}
+// Symbol
+//*
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return num1 + num2;
+}
+var res = add(45, 67);
+console.log(res);
--- Typescript Workspace/1_Datatypes in Typescript/datatypes.ts
+++ Typescript Workspace/1_Datatypes in Typescript/datatypes.ts
@@ -17,6 +17,48 @@ employeeId="e890";
 let ss : string | number;
 
 let branch="Rabale";
+
 //branch=8999;
 
 let contactNumber;contactNumber=7878787878;
+
+let headoffice;
+headoffice="Dadar";
+console.log((<string>headoffice).length);
+console.log(headoffice.charAt(0));
+
+let sss:string;
+sss="hello";
+
+let employee={
+    id:567,
+    name:"hari",
+    salary:78000
+}
+console.log(employee.id);
+console.log(employee.name);
+console.log(employee.salary);
+
+for(let key in employee){
+    console.log(key);
+    console.log(employee[key]);
+    
+}
+// Symbol
+//*
+
+
+function add(num1:number,num2:number):number
+{
+    console.log(num1+num2);
+    return num1+num2;
+}
+
+let res=add(45,67);
+console.log(res);
+
+
+function add2(num1:number,num2:number):void
+{
+    console.log(num1+num2);
+}
