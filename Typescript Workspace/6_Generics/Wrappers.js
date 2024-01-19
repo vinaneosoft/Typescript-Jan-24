741a9a1ec7230ecda58a7efb7505f9b38cfc5a41
kokanevina <kokane.vina@gmail.com>
2024-01-19 17:21:11 +0530

    Wrappers

--- Typescript Workspace/6_Generics/Wrappers.js
+++ Typescript Workspace/6_Generics/Wrappers.js
@@ -14,3 +14,31 @@ var ss = s2.valueOf();
 console.log(ss);
 console.log(typeof ss);
 console.log(s2.valueOf() === s3.valueOf());
+var Employee = /** @class */ (function () {
+    function Employee(id, salary) {
+        if (id === void 0) { id = 0; }
+        if (salary === void 0) { salary = 0; }
+        this.employeeId = id;
+        this.employeeSalary = salary;
+    }
+    return Employee;
+}());
+var e1 = new Employee(1234, 56000);
+console.log(e1.employeeId);
+console.log(typeof e1.employeeId);
+console.log(e1.employeeSalary);
+console.log(typeof e1.employeeSalary);
+console.log(e1);
+var num = 78;
+console.log(typeof num);
+var num2 = new Number(78);
+console.log(typeof num2);
+var num3 = num2;
+console.log(typeof num3);
+console.log(num3);
+var num4;
+//num4=num2;
+var num5 = 890;
+var num6;
+num6 = num5;
+console.log(typeof num6);
--- Typescript Workspace/6_Generics/Wrappers.ts
+++ Typescript Workspace/6_Generics/Wrappers.ts
@@ -21,3 +21,39 @@ console.log(ss);
 console.log(typeof ss);
 
 console.log(s2.valueOf()===s3.valueOf());
+
+
+class Employee{
+    employeeId:Number
+    employeeSalary:number
+    constructor(id:Number=0,salary=0){
+        this.employeeId=id;
+        this.employeeSalary=salary
+    }
+}
+let e1=new Employee(new Number(1234),56000)
+console.log(e1.employeeId);
+console.log(typeof e1.employeeId);
+console.log(e1.employeeSalary);
+console.log(typeof e1.employeeSalary);
+console.log(e1);
+
+
+let num:Number=78;
+console.log(typeof num);
+
+let num2=new Number(78);
+console.log(typeof num2);
+
+let num3=num2;
+console.log(typeof num3);
+console.log(num3);
+let num4:number;
+//num4=num2;
+
+let num5=890;
+let num6:Number
+num6=num5;
+console.log(typeof num6);
+
+let numObject=new Number(num5);
