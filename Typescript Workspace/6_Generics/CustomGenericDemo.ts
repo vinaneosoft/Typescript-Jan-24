b43766377b675c2bca4f1f498bc4b878ef06a05b
kokanevina <kokane.vina@gmail.com>
2024-01-23 16:20:50 +0530

    custom generic class

---
+++ Typescript Workspace/6_Generics/CustomGenericDemo.ts
@@ -0,0 +1,23 @@
+class NeoEmployee<E>{
+    employeeId:E
+    employeeName:string;
+    // not set get properties. these are functions written to show typesafety
+    getEmployeeId():E{
+        return this.employeeId
+    }
+    setEmployeeId(employeeId:E):void{
+        this.employeeId=employeeId
+    }
+}
+
+let employee1=new NeoEmployee<string>();
+employee1.employeeId="890";
+//employee1.employeeId=567;
+console.log(employee1.getEmployeeId());
+let employee2=new NeoEmployee<number>();
+employee2.employeeId=888;
+//employee2.employeeId="777";
+console.log(employee2.getEmployeeId());
+
+employee1.setEmployeeId("1222222");
+employee2.setEmployeeId(556);
